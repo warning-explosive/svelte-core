ede5eca45143ff22652f87742c83f77b49e1bcd6
nikitagrishin <nikitagrishin1993@yandex.ru>
2021-12-02 23:10:13 +0300

    recode data-grid to support svelte animations

--- src/components/DataGrid/DataGrid.svelte
+++ src/components/DataGrid/DataGrid.svelte
@@ -1,65 +1,202 @@
 <script lang="ts">
     import {createEventDispatcher} from "svelte";
+    import {flip, FlipParams} from "svelte/animate";
     import {createGridStore} from "../../scripts/gridStore";
-    import DataRow from "./DataRow.svelte";
-    import type {Entity} from "../../scripts/containers";
+    import type {Entity, StringContainer} from "../../scripts/containers";
     import type {SwapColumnsData} from "../../scripts/dataGrid";
+    import {Containers} from "../../scripts/containers";
+    import {ColumnData} from "../../scripts/dataGrid";
 
     export let url: string;
 
     const dispatch = createEventDispatcher();
 
-    let selected:string[] = [];
+    /*
+     * Row selection
+     */
+    let selected: string[] = [];
 
-    const select = (event: CustomEvent<string>): void => {
-        selected.push(event.detail);
-        selected = selected;
+    const isSelected = (entity: Entity, selected: string[]): boolean => {
+        return selected.includes(entity.id.value);
     };
 
-    const unselect = (event: CustomEvent<string>): void => {
-        selected = selected.filter(id => id !== event.detail);
+    const select = (entity: Entity): void => {
+        const id = entity.id.value;
+        if (selected.includes(id)) {
+            selected = selected.filter(it => it !== id);
+        } else {
+            selected = [...selected, id];
+        }
     };
 
-    const { store, refresh, getById, reorderColumns } = createGridStore(url);
+    /*
+     * Grid store
+     */
+    const { store, refresh, reorderColumns } = createGridStore(url);
 
     const refreshDataGrid = () => {
         selected = [];
         refresh();
     }
 
-    const openForm = (event: CustomEvent<string>): void => {
-        dispatch('openForm', getById(event.detail));
+    /*
+     * Form opening
+     */
+    const openForm = (item: Entity): void => {
+        dispatch('openForm', item);
     }
 
-    const swapColumns = (event: CustomEvent<SwapColumnsData>) => {
-        reorderColumns(event.detail);
+    /*
+     * Display data
+     */
+    const getHeaderContainer = (key: string): StringContainer => {
+        return { kind: 'string', value: key };
     };
 
-    let headerContainers: Entity = {
-        id: {
-            kind: 'string',
-            value: 'id'
+    const getDisplayValue = (container: Containers): string => {
+        let displayValue: string;
+
+        switch (container.kind) {
+            case "string":
+                displayValue = container.value;
+                break;
+            case "number":
+                displayValue = container.value.toString();
+                break;
+            case "boolean":
+                displayValue = container.value.toString();
+                break;
+            case "date":
+                displayValue = container.value.toDateString();
+                break
+            case "link":
+                displayValue = `${container.caption} (${container.id})`;
+                break;
+            case "money":
+                displayValue = `${container.amount} ${container.currency}`;
+                break;
+            default:
+                const _exhaustiveCheck: never = container;
+                break;
         }
-    };
 
-    const getHeaderContainers = (acc: Entity, key: string): Entity => {
-        acc[key] = { kind: 'string', value: key };
-        return acc;
-    };
+        return displayValue;
+    }
+
+    /*
+     * Drag & Drop
+     */
+    let dndHoveredKey: string | undefined = undefined;
+
+    const dataKey = 'text/plain';
+
+    const getColumnData = (key: string, index: number): ColumnData => {
+        return {
+            key: key,
+            index: index
+        };
+    }
+
+    const onDragStart = (key:string, index: number, event: DragEvent): void => {
+        event.dataTransfer.setData(dataKey, JSON.stringify(getColumnData(key, index)));
+    }
+
+    const onDragEnter = (key: string): void => {
+        dndHoveredKey = key;
+    }
+
+    const onDragLeave = (): void => {
+        dndHoveredKey = undefined;
+    }
+
+    const onDragOver = (event: DragEvent): void => {
+        event.dataTransfer.dropEffect = 'move';
+    }
+
+    const onDrop = (key: string, index: number, event: DragEvent): void => {
+        const columnData: ColumnData = JSON.parse(event.dataTransfer.getData(dataKey));
+
+        if (columnData.key !== key) {
+            const swapColumnsData: SwapColumnsData = { from: columnData, to: getColumnData(key, index) }
+            reorderColumns(swapColumnsData);
+        }
+
+        dndHoveredKey = undefined;
+    }
+
+    /*
+     * Animation
+     */
+    const flipParams: FlipParams = { duration: 500 };
 </script>
 
-<button on:click={refreshDataGrid} disabled={$store.state !== 'idling'}>{$store.state === "loading" ? 'Loading...' : 'Refresh'}</button>
+<button
+    disabled={$store.state !== 'idling'}
+    on:click={refreshDataGrid}>
+    {$store.state === "loading" ? 'Loading...' : 'Refresh'}
+</button>
 <span>Selected rows: {selected.length ? selected : 'empty'}</span>
 {#if $store.state === "error"}
     <span>Error: {$store.data}</span>
 {:else if $store.state === "idling"}
     <table>
-        <DataRow id={'header'} isHeader={true} keys={$store.keys} containers={$store.keys.reduce(getHeaderContainers, headerContainers)} on:swapColumns={swapColumns} />
-        {#each $store.data as containers (containers.id.value)}
-            <DataRow id={containers.id.value} isHeader={false} keys={$store.keys} {containers} on:selected={select} on:unselected={unselect} on:openForm={openForm} />
+        <tr class="header">
+            {#each $store.keys as key, index (key)}
+                <th
+                    class="noselect"
+                    class:dndHover={dndHoveredKey === key}
+                    animate:flip={flipParams}
+                    draggable={true}
+                    on:dragstart={e => onDragStart(key, index, e)}
+                    on:dragover|preventDefault={onDragOver}
+                    on:drop={e => onDrop(key, index, e)}
+                    on:dragenter={() => onDragEnter(key)}
+                    on:dragleave={onDragLeave}>
+                    {getDisplayValue(getHeaderContainer(key))}
+                </th>
+            {/each}
+        </tr>
+        {#each $store.data as entity (entity.id.value)}
+            <tr class:selected={isSelected(entity, selected)} on:click={() => select(entity)} on:dblclick={() => openForm(entity)}>
+                {#each $store.keys as key, index (key)}
+                    <td
+                        class="noselect"
+                        animate:flip={flipParams}>
+                        {getDisplayValue(entity[key])}
+                    </td>
+                {/each}
+            </tr>
         {/each}
     </table>
 {/if}
 
 <style>
+    table {
+        border-collapse: collapse;
+    }
+
+    th, td {
+        text-align: left;
+        border-bottom: 1px solid black;
+    }
+
+    tr.selected {
+        background-color: cornflowerblue;
+    }
+
+    tr:hover:not(.header) {
+        background-color: darkgrey;
+    }
+
+    tr.selected:hover:not(.header) {
+        background-color: royalblue;
+    }
+
+    th[draggable] {
+        cursor: grab;
+    }
+
+    .dndHover {
+        background-color: darkgrey;
+    }
 </style>
